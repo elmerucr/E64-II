a7b3fec40a8bad29c44523319117e2fffca001d9
elmerucr <elmerucr@gmail.com>
2020-09-20 12:20:24 +0200

    settings management

--- src/resources/settings.cpp
+++ src/resources/settings.cpp
@@ -11,8 +11,20 @@
 
 E64::settings::settings()
 {
-    snprintf(settings_path, 256, "%s/.E64-II", getenv("HOME"));
-    printf("trying to open settings directory: %s\n",settings_path);
+    snprintf(settings_path, 256, "%s", getenv("HOME"));     // within app bundle returns too much
+    
+    char *iterator = settings_path;
+    int times = 0;
+    while(*iterator != '\0')                    // shorten the path name to home and user dir
+    {
+        if(*iterator == '/') times++;
+        if(times == 3) *iterator = '\0';
+        iterator++;
+    }
+    
+    snprintf(settings_path, 256, "%s/.E64-II", settings_path);
+    
+    printf("[Settings] trying to open settings directory: %s\n",settings_path);
 }
 
 E64::settings::~settings()
