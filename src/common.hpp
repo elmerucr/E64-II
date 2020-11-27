7fd17ac84a016aaf99dd9f6181a5035cf0280e4a
elmerucr <elmerucr@gmail.com>
2020-11-27 21:15:55 +0100

    rom update

--- src/common.hpp
+++ src/common.hpp
@@ -16,7 +16,7 @@
 
 #define E64_II_MAJOR_VERSION    0
 #define E64_II_MINOR_VERSION    5
-#define E64_II_BUILD            20201123
+#define E64_II_BUILD            20201127
 #define E64_II_YEAR             2020
 
 /* Global objects */
